5c7a9d504f99a543169acb5a55445f096a472cbd
namiqabasov <7td508c@code.edu.az>
2023-05-12 16:37:22 +0400

    yeni taskt

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,5 +1,20 @@
-fetch("https://restcountries.com/v3.1/all")
-  .then(res => res.json())
-  .then(data => {
-    data.forEach((element, i) => {});
-  });
+const myCarts = document.querySelector(".myCarts");
+
+async function Myfetch() {
+  const response = await fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((element, i) => {
+        console.log(element);
+      });
+    });
+}
+
+Myfetch();
+
+function createCartelement(data) {
+  const myDiv = document.createElement("div");
+  const myPtag1 = document.createElement("p");
+  const myPtag2 = document.createElement("p");
+
+}
--- assets/js/select.js
+++ assets/js/select.js
@@ -1,5 +1,20 @@
-const btnSelect = document.querySelector(".btn-select");
+const btnSelect = document.querySelectorAll(".btn-select");
+const aDigital = document.querySelectorAll(".aDigital");
+const canProvid = document.querySelectorAll(".canProvid");
 
-for (let i = 0; i < btnSelect.length; i++) {
-  btnSelect[i].addEventListener("click", function () {});
-}
+console.log(canProvid);
+
+btnSelect.forEach((element) => {
+  element.addEventListener("click", function () {
+    btnSelect.forEach((btn) => {
+      console.log(btn.parentElement.nextElementSibling);
+      btn.parentElement.nextElementSibling.style.display = "none";
+    });
+
+    element.parentElement.nextElementSibling.style.display = "block";
+    element.parentElement.parentElement.style.cssText = `
+    background-color: white;
+    `;
+    
+  });
+});
